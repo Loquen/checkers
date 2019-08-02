200ec789a37a17f8ea768bd7a2c373cf839ba306
Loquen Jones <lrjones416@gmail.com>
2019-08-02 14:01:55 -0700

    boilerplate js and init function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,45 @@
-document.querySelector('section').addEventListener('click', function(evt){
-    console.log(evt.target);
-    evt.target.className = 'highlight';
-});
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+let board, winner, turn;
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+init();
+
+function init(){
+  // Initialize the board with both players
+  board = [
+    0,1,0,1,0,1,0,1,
+    1,0,1,0,1,0,1,0,
+    0,1,0,1,0,1,0,1,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    -1,0,-1,0,-1,0,-1,0,
+    0,-1,0,-1,0,-1,0,-1,
+    -1,0,-1,0,-1,0,-1,0
+  ];
+  // Set turn to Player 1
+  turn = 1;
+  // Set winner to null (1, -1, 'T' and null)
+  winner = null;
+  // Call render() to refresh the state
+  render();
+}
+
+function render(){
+  // Loop through board array and render each cell as empty or peon
+  // Grab each element/div 
+  // set style of inner div element to  
+
+}
+
+function handleClick(){
+
+}
