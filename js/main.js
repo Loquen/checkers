a84d2d0cb84db41e282d38c14dfc2c4f0d4d43e6
Loquen Jones <lrjones416@gmail.com>
2019-08-02 15:37:00 -0700

    render initial board works

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 /*----- constants -----*/
-
+const validSquares = [
+  1,3,5,7,
+  8,10,12,14,
+  17,19,21,23,
+  24,26,28,30,
+  33,35,37,39,
+  40,42,44,46,
+  49,51,53,55,
+  56,58,60,62
+]
 
 /*----- app's state (variables) -----*/
 let board, winner, turn;
@@ -37,7 +46,16 @@ function render(){
   // Loop through board array and render each cell as empty or peon
   // Grab each element/div 
   // set style of inner div element to  
-
+  board.forEach(function(cell,idx){
+    // If the board has a peon and that square is valid 
+    // then we can grab the square and render
+    if(board[idx] !== 0 && validSquares.includes(idx)){
+      let div = document.getElementById(idx.toString());
+      div = board[idx] === 1 ? 
+        div.classList.add('white') : div.classList.add('black');
+    }
+     
+  });
 }
 
 function handleClick(){
